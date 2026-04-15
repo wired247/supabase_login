05b95f31b190aba99fbf9a32bb8488ac7b07443f
wired247 <willoughby.d@gmail.com>
2026-04-15 12:54:02 -0400

    Unused import prevents deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import '@aws-amplify/ui-react/styles.css';
 import OAuthConsent from './OAuthConsent';
